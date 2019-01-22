bc92e128f6a21fc11ea86d3b7597ec1dd51de7d6
Aaron Smulktis <aaronsmulktis@gmail.com>
2019-01-21 22:04:20 -0800

    try getting everything running in subfolder

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,22 @@ class ComposerStaticInit3266d845da9528ce76fdffd012278384
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'j' => 
+        array (
+            'johnpbloch\\Composer\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/johnpbloch/wordpress-core-installer/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit3266d845da9528ce76fdffd012278384::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit3266d845da9528ce76fdffd012278384::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit3266d845da9528ce76fdffd012278384::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
